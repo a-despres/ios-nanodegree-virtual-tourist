8ec000487fa624d897fe3cc712af507fc051ab56
Andrew Despres <21027320+a-despres@users.noreply.github.com>
2019-01-07 20:16:56 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# iOS Developer Nanodegree - Project 4 - Virtual Tourist
---
+++ ios-nanodegree-virtual-tourist/View Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ios-nanodegree-virtual-tourist
+//
+//  Created by Andrew Despres on 1/7/19.
+//  Copyright © 2019 Andrew Despres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - View Controller
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
